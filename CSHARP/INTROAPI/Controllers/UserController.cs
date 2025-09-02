1620b222efcdf7bcd33345012e990450c8d351ad
Rodrigo081pb <kauarodrigo1193@gmail.com>
2025-09-02 00:04:03 -0300

    Introdução ao APIWEB com c#

---
+++ CSHARP/INTROAPI/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace INTROAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet("ObterDataHora")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+            {
+
+                DataHoraAtual = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToLongTimeString()
+            };
+
+            return Ok(obj);
+        }
+
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var menssagem = $"Olá {nome}, seja bem vindo(a)!";
+            return Ok(new { menssagem });
+        }
+
+    }
+}
